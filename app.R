05e863c1e6501773ca3b156b0815e4315194a78a
nickhir <nick.hirschmueller@yahoo.de>
2024-09-25 14:48:37 +0100

    Update Abstract and Title

--- app.R
+++ app.R
@@ -429,14 +429,14 @@ ui <- dashboardPage(
             #############
             tabItem(
                 tabName = "about",
-                h4("Proliferation and differentiation of intestinal stem cells depends on the zinc finger transcription factor BCL11/Chronophage",
+                h4("Intestinal stem cell proliferation and differentiation depends on the self-repressive zinc finger transcription factor BCL11/Cph",
                     align = "center",
                     style = "width: 80%;  margin: 0 auto; font-weight: bold; font-style: italic;"
                 ),
                 div(style = "margin-bottom: 10px;"), # Add a margin to create space
 
                 h5(HTML("Siamak Redhai*<sup>#</sup>, Nick Hirschmüller*, Tianyu Wang, Shivohum Bahuguna,
-              Svenja Leible, Stefan Peidli, Erica Valentani1, Sviat Kharuk, Michaela Holemz, Lea Bräckow,
+              Svenja Leible, Stefan Peidli, Erica Valentani, Sviat Kharuk, Michaela Holemz, Lea Bräckow,
                         Fillip Port, David Ibberson, Wolfgang Huber<sup>#</sup>, Michael Boutros<sup>#</sup>"),
                     align = "center",
                     style = "width: 60%;  margin: 0 auto; color: darkgrey;"
@@ -449,7 +449,7 @@ ui <- dashboardPage(
                 ),
                 div(style = "margin-bottom: 30px;"), # Add a margin to create space
                 h3("Abstract", style = "font-weight: bold"),
-                p("The  molecular  programs  that  drive  proliferation  and  differentiation  of  intestinal  stem  cells (ISCs) are essential for organismal fitness. Notch signalling regulates the binary fate decision of ISCs, favouring enterocyte commitment when Notch activity is high and enteroendocrine cell (EE) fate when activity is low. However, the gene regulatory mechanisms that underlie this process on an organ scale remain poorly understood. Here, we find that the expression of the  C2H2-type  zinc-finger  transcription  factor  Chronophage  (Cph),  homologous  to mammalian  BCL11,  increases  specifically  along  the  ISC-to-EE  lineage  when  Notch  is inactivated. We show that the expression of Cph is regulated by the Achaete-Scute Complex (AS-C) gene, scute, which directly binds to multiple sites within the Cph locus to promote its expression. Our genetic and single-cell RNA sequencing experiments demonstrate that Cph maintains  the  ISC  and  EE  populations  and  is  necessary  to  remodel  the  transcriptome  of progenitor cells with low Notch activity. By identifying and functionally validating Cph target genes, we uncover a novel role for sugar free frosting (sff) in directing proliferative and lineage commitment steps of ISCs. Our findings provide mechanistic insight into how Cph maintains intestinal epithelial homeostasis, and together with our evolutionary analysis of Cph/BCL11, suggests that it may represent a general mechanism used by adult somatic stem cells in other tissues to balance proliferation and differentiation."),
+                p("The molecular programs that drive proliferation and differentiation of intestinal stem cells (ISCs) are essential for organismal fitness. Notch signalling regulates the binary fate decision of ISCs, favouring enterocyte commitment when Notch activity is high and enteroendocrine cell (EE) fate when activity is low. However, the temporal dynamics of transcription factor expression and its involvement in generating distinct EE lineages across different intestinal regions remain largely uncharacterised. Here, we find that the expression of the C2H2-type zinc-finger transcription factor Chronophage (Cph), homologous to mammalian BCL11, increases early along the ISC-to-EE lineage when Notch is inactivated. We show that the expression of Cph is regulated by the Achaete-Scute Complex (AS-C) gene, scute, which directly binds to multiple sites within the Cph locus to promote its expression. Our genetic and single-cell RNA sequencing experiments demonstrate that Cph maintains the ISC and EE populations across different regions and is required to remodel the transcriptome of ISCs with low Notch activity. By performing in vivo chromatin profiling, we find that Cph activates the expression of key genes involved in proliferation and lineage commitment, while simultaneously repressing its own expression. This inhibitory feedback loop prevents ISCs from undergoing autophagy-dependent cell death and ensures differentiation is faithfully executed. Our findings shed light on the mechanism by which Cph sustains intestinal epithelial homeostasis and could represent a conserved strategy for balancing proliferation and differentiation in other tissues and species."),
                 p(
                     "This Shiny App allows users to interactively explore the datasets accompanying the publication.",
                     "The source code for the app can be found on ", a("GitHub", href = "https://github.com/nickhir/IntestiMap"), "."
